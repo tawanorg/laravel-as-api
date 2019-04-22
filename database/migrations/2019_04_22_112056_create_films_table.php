f67df0d37d24720b73a52eb7da7c40a2ea3c19f2
Tim Tawan <mrtawans@gmail.com>
2019-04-22 21:31:39 +1000

    added migrate database

---
+++ database/migrations/2019_04_22_112056_create_films_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug');
+            $table->string('name');
+            $table->longText('description');
+            $table->date('realeasedate');
+            $table->integer('rating');
+            $table->string('ticketprice');
+            $table->string('country');
+            $table->json('genre');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
